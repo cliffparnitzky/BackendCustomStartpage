0251c2d6611d04c75bef6eaa3a5a861288101c6b
Cliff Parnitzky <github@cliff-parnitzky.de>
2014-06-04 00:23:48 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,30 @@
+Contao Extension: BackendCustomStartpage
+========================================
+
+Provides the possibility for each user to select an individual startpage in the backend.
+
+
+Installation
+------------
+
+The extension is not published in contao extension repository.
+Install it manually or via [composer](https://packagist.org/packages/cliffparnitzky/backend-custom-startpage).
+
+
+Tracker
+-------
+
+https://github.com/cliffparnitzky/BackendCustomStartpage/issues
+
+
+Compatibility
+-------------
+
+- min. version: Contao 2.9.5
+- max. version: Contao 3.3.x
+
+
+Dependency
+----------
+
+- There are no dependencies to other extensions, that have to be installed.
---
+++ system/modules/BackendCustomStartpage/BackendCustomStartpageHook.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2014 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Cliff Parnitzky 2014
+ * @author     Cliff Parnitzky
+ * @package    BackendCustomStartpage
+ * @license    LGPL
+ */
+
+/**
+ * Class BackendCustomStartpageHook
+ *
+ * Special hook implementation for redirect to the custom startpage
+ * @copyright  Cliff Parnitzky 2014
+ * @author     Cliff Parnitzky
+ * @package    Controller
+ */
+class BackendCustomStartpageHook extends Backend 
+{
+	/**
+	 * Execute postLogin hook to redirect to the custom startpage
+	 */
+	public function redirectToCustomStartpage (User $objUser)
+	{
+		if ($objUser instanceof BackendUser && (strlen($objUser->backendCustomStartpage) > 0) && ($objUser->isAdmin || $objUser->hasAccess('modules', $objUser->backendCustomStartpage)))
+		{
+			$this->redirect('contao/main.php?do=' . $objUser->backendCustomStartpage); 
+		}
+	}
+}
+?>
